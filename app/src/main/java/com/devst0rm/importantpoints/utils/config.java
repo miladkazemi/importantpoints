36476fb2ef95bd758eb19e8f2adbb70a62d7c775
MiladStOrM <51346645+miladkazemi@users.noreply.github.com>
2024-08-04 05:06:54 +0330

    Update config.java

--- app/src/main/java/com/devst0rm/importantpoints/utils/config.java
+++ app/src/main/java/com/devst0rm/importantpoints/utils/config.java
@@ -9,9 +9,6 @@ public class config {
     private final static String mainIP = "192.168.21.3"; // local
     public final static String mainIP_API = "http://" + mainIP + ":8000"; // local
 //   public final static String mainIP_FILES = "http://192.168.230.3:8000/"; // local
-
-    //    public final static String mainIP_FILES = "https://api.devstorm.pro/"; // sv
-//    public final static String mainIP_API = "http://45.156.184.62:8000"; // sv
     public final static String APIVersion_1 = "/api/v1/";
 
 
